3699be6a5a9aa5e413a58fa0dbf33c404d03a6be
RedIce-X <raunaknaik99@gmail.com>
2021-11-16 20:16:05 +0530

    moved resources folder to src/test and created testng.xml file

--- src/test/java/com/team3/miniproject/base/TestBase.java
+++ src/test/java/com/team3/miniproject/base/TestBase.java
@@ -1,5 +1,29 @@
 package com.team3.miniproject.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 public class TestBase {
-	
+	public WebDriver driver;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeTest(String browser) {
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+	}
+
+	@AfterClass
+	public void afterTest() {
+		driver.quit();
+
+	}
+
 }
--- src/test/java/com/team3/miniproject/testcases/ddt/RegistrationData.java
+++ src/test/java/com/team3/miniproject/testcases/ddt/RegistrationData.java
@@ -1,7 +1,6 @@
 package com.team3.miniproject.testcases.ddt;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -10,32 +9,31 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class RegistrationData {
 
-	public ArrayList<ArrayList<String>> userData() throws IOException{
-		ArrayList<ArrayList<String>> userdata=new ArrayList<ArrayList<String>>(5);
-		
-		
-		FileInputStream fis = new FileInputStream("Resources\\RegistrationData.xlsx");
+	public ArrayList<ArrayList<String>> userData() throws IOException {
+		ArrayList<ArrayList<String>> userdata = new ArrayList<ArrayList<String>>(5);
+
+		FileInputStream fis = new FileInputStream("src\\test\\resources\\RegistrationData.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		try {
-		for(int i=0;i<5;i++) {
-			userdata.add(new ArrayList());
-			ArrayList<String> iuserdata=new ArrayList<String>(6);
-			for(int j=0;j<6;j++) {
-				if(sheet.getRow(i).getCell(j)==null) {
-					iuserdata.add("");
-				}else {
-				iuserdata.add(sheet.getRow(i).getCell(j).toString());
+			for (int i = 0; i < 5; i++) {
+				userdata.add(new ArrayList());
+				ArrayList<String> iuserdata = new ArrayList<String>(6);
+				for (int j = 0; j < 6; j++) {
+					if (sheet.getRow(i).getCell(j) == null) {
+						iuserdata.add("");
+					} else {
+						iuserdata.add(sheet.getRow(i).getCell(j).toString());
+					}
 				}
+				userdata.get(i).addAll(iuserdata);
 			}
-			userdata.get(i).addAll(iuserdata);
-		}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return userdata;
 	}
-	
+
 //	public static void main(String args[]) throws IOException {
 //		RegistrationData rd=new RegistrationData();
 //		ArrayList<ArrayList<String>> myData=rd.userData();
