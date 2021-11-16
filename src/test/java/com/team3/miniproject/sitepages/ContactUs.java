4169103a7c9dc6f2dcfeb7bad0fc6cd1912d4d62
RedIce-X <raunaknaik99@gmail.com>
2021-11-16 23:13:56 +0530

    Coded TC_OC_CU_003, 004, 005

--- src/test/java/com/team3/miniproject/sitepages/ContactUs.java
+++ src/test/java/com/team3/miniproject/sitepages/ContactUs.java
@@ -1,57 +1,86 @@
 package com.team3.miniproject.sitepages;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ContactUs {
-	
+
 	WebDriver driver;
-	
-	//name field element
-	@FindBy(id="input-name")
+
+	// name field element
+	@FindBy(id = "input-name")
 	WebElement nameField;
-	
-	//email field element
-	@FindBy(id="input-email")
+
+	// email field element
+	@FindBy(id = "input-email")
 	WebElement emailField;
-	
-	//enquiry field element
-	@FindBy(id="input-enquiry")
+
+	// enquiry field element
+	@FindBy(id = "input-enquiry")
 	WebElement enquiryField;
-	
-	//submit button element
+
+	// submit button element
 	@FindBy(xpath = "//input[@value='Submit']")
 	WebElement submitBtn;
-	
+
+	// your name field warning (appears if input is not valid)
+	@FindBy(xpath = "//*[@id='content']/form/fieldset/div[1]/div/div")
+	WebElement yourNameWarning;
+
+	// E-mail address field warning (appears if input is not valid)
+	@FindBy(xpath = "//*[@id='content']/form/fieldset/div[2]/div/div")
+	WebElement emailWarning;
+
+	// Enquiry field warning (appears if input is not valid)
+	@FindBy(xpath = "//*[@id='content']/form/fieldset/div[3]/div/div")
+	WebElement enquiryWarning;
+
 	public ContactUs(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	//method to enter name
+
+	// method to enter name
 	public void enterName(String name) {
 		nameField.sendKeys(name);
 	}
-	
-	//method to enter email
+
+	// method to enter email
 	public void enterEmail(String email) {
 		emailField.sendKeys(email);
 	}
-	
-	//method to enter enquiry
+
+	// method to enter enquiry
 	public void enterEnquiry(String enquiry) {
 		enquiryField.sendKeys(enquiry);
 	}
-	
-	//method to click submit
+
+	// method to click submit
 	public void clickSubmit() {
 		submitBtn.click();
 	}
 
-	//method to get page title
+	// method to get page title
 	public String getPageTitle() {
-		String pageTitle=driver.getTitle();
+		String pageTitle = driver.getTitle();
 		return pageTitle;
 	}
+
+	public String getYourNameWarning() {
+		String warningText = yourNameWarning.getText();
+		return warningText;
+	}
+
+	public String getEmailWarning() {
+		String warningText = emailWarning.getText();
+		return warningText;
+	}
+
+	public String getEnquiryWarning() {
+		String warningText = enquiryWarning.getText();
+		return warningText;
+	}
+
 }
--- src/test/java/com/team3/miniproject/testcases/contactus/ContactUsTest.java
+++ src/test/java/com/team3/miniproject/testcases/contactus/ContactUsTest.java
@@ -17,64 +17,96 @@ import com.team3.miniproject.sitepages.ContactUs;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ContactUsTest {
-	
-	WebDriver driver;
-	String baseUrl="http://localhost/opencartsite/index.php?route=information/contact";
-	ContactUs cnct_object;
-	
-	
-  @Test(enabled=false)
-  public void testCase006() {
-	  cnct_object=new ContactUs(driver);
-	  cnct_object.enterName("Errol");
-	  cnct_object.enterEmail("demo@email.com");
-	  cnct_object.enterEnquiry("Testing if this field is working without");
-	  cnct_object.clickSubmit();
-  }
-  
-  @Test(enabled=false)
-  public void testCase007() {
-	  try {
-			JavascriptExecutor js=(JavascriptExecutor)driver;
-			String astricks1=(String)js.executeScript("return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(2) > label.control-label\"),':before').getPropertyValue('content')");
-			String char1=Character.toString(astricks1.charAt(1));
-			Assert.assertEquals("*",char1);
-			
-			String astricks2=(String)js.executeScript("return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(3) > label.control-label\"),':before').getPropertyValue('content')");
-			String char2=Character.toString(astricks2.charAt(1));
-			Assert.assertEquals("*", char2);
-			
-			String astricks3=(String)js.executeScript("return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(4) > label.control-label\"),':before').getPropertyValue('content')");
-			String char3=Character.toString(astricks3.charAt(1));
-			Assert.assertEquals("*", char3);
-			
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-  }
-  
-  @Test
-  public void testCase008() {
-		try {
-		WebElement nameField=driver.findElement(By.id("input-name"));
-		Assert.assertEquals("", nameField.getText());
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-  }
-  
-  @BeforeMethod
-  public void beforeMethod() {
-	  WebDriverManager.chromedriver().setup();
-	  driver=new ChromeDriver();
-	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	  driver.get(baseUrl);
-	  driver.manage().window().maximize();
-  }
 
-  @AfterMethod
-  public void afterMethod() {
-	  driver.quit();
-  }
+	WebDriver driver;
+	String baseUrl = "http://localhost/opencartsite/index.php?route=information/contact";
+	ContactUs cnct_object;
+
+	@Test(enabled = true)
+	public void testCase003() {
+		cnct_object = new ContactUs(driver);
+		cnct_object.enterName("Tester");
+		cnct_object.enterEmail("demo1@example");
+		cnct_object.enterEnquiry("When will the iPad Air be in stock again");
+		cnct_object.clickSubmit();
+		Assert.assertEquals(cnct_object.getEmailWarning(), "E-Mail Address does not appear to be valid!");
+	}
+
+	@Test(enabled = true)
+	public void testCase004() {
+		cnct_object = new ContactUs(driver);
+		cnct_object.enterName("Te");
+		cnct_object.enterEmail("demo@email.com");
+		cnct_object.enterEnquiry("When will the iPad Air be in stock again");
+		cnct_object.clickSubmit();
+		Assert.assertEquals(cnct_object.getYourNameWarning(), "Name must be between 3 and 32 characters!");
+	}
+
+	@Test(enabled = true)
+	public void testCase005() {
+		cnct_object = new ContactUs(driver);
+		cnct_object.enterName("Tester");
+		cnct_object.enterEmail("demo@email.com");
+		cnct_object.enterEnquiry("abcd");
+		cnct_object.clickSubmit();
+		Assert.assertEquals(cnct_object.getEnquiryWarning(), "Enquiry must be between 10 and 3000 characters!");
+	}
+
+	@Test(enabled = false)
+	public void testCase006() {
+		cnct_object = new ContactUs(driver);
+		cnct_object.enterName("Errol");
+		cnct_object.enterEmail("demo@email.com");
+		cnct_object.enterEnquiry("Testing if this field is working without");
+		cnct_object.clickSubmit();
+	}
+
+	@Test(enabled = false)
+	public void testCase007() {
+		try {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			String astricks1 = (String) js.executeScript(
+					"return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(2) > label.control-label\"),':before').getPropertyValue('content')");
+			String char1 = Character.toString(astricks1.charAt(1));
+			Assert.assertEquals("*", char1);
+
+			String astricks2 = (String) js.executeScript(
+					"return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(3) > label.control-label\"),':before').getPropertyValue('content')");
+			String char2 = Character.toString(astricks2.charAt(1));
+			Assert.assertEquals("*", char2);
+
+			String astricks3 = (String) js.executeScript(
+					"return window.getComputedStyle(document.querySelector(\"fieldset > div.required:nth-child(4) > label.control-label\"),':before').getPropertyValue('content')");
+			String char3 = Character.toString(astricks3.charAt(1));
+			Assert.assertEquals("*", char3);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test(enabled = false)
+	public void testCase008() {
+		try {
+			WebElement nameField = driver.findElement(By.id("input-name"));
+			Assert.assertEquals("", nameField.getText());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
 
 }
