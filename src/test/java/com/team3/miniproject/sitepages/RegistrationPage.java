ae97c26b4f86bf7597eae7ead43acacd14ad8232
RedIce-X <raunaknaik99@gmail.com>
2021-11-16 22:11:24 +0530

    Modified TEST_REG_011

--- src/test/java/com/team3/miniproject/sitepages/RegistrationPage.java
+++ src/test/java/com/team3/miniproject/sitepages/RegistrationPage.java
@@ -8,55 +8,53 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 public class RegistrationPage {
-	
+
 	WebDriver driver;
-	
-	//first name input field element 
-	@FindBy(id="input-firstname")
-	WebElement firstNameInput; 
-	
-	//last name input field element
-	@FindBy(id="input-lastname")
+
+	// first name input field element
+	@FindBy(id = "input-firstname")
+	WebElement firstNameInput;
+
+	// last name input field element
+	@FindBy(id = "input-lastname")
 	WebElement lastNameInput;
-	
-	//email input field element
-	@FindBy(id="input-email")
+
+	// email input field element
+	@FindBy(id = "input-email")
 	WebElement emailInput;
-	
-	//telephone input field element
-	@FindBy(id="input-telephone")
+
+	// telephone input field element
+	@FindBy(id = "input-telephone")
 	WebElement telephoneInput;
-	
-	
-	//password input field element
-	@FindBy(id="input-password")
+
+	// password input field element
+	@FindBy(id = "input-password")
 	WebElement passwordInput;
-	
-	
-	//confirm password input field element
-	@FindBy(id="input-confirm")
+
+	// confirm password input field element
+	@FindBy(id = "input-confirm")
 	WebElement confirmPasswordInput;
-	
-	
-	//privacy checkbox element
+
+	// privacy checkbox element
 	@FindBy(xpath = "//input[@name='agree']")
 	WebElement privacyCheckbox;
-	
-	//continue button element
+
+	// continue button element
 	@FindBy(xpath = "//input[@value='Continue']")
 	WebElement continueBtn;
-	
-	//warning div element
+
+	// warning div element
 	@FindBy(xpath = "//div[@class='alert alert-danger alert-dismissible']")
 	WebElement warningDiv;
-	
+
 	public RegistrationPage(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	//method to fill registration form
-	public void fillRegistrationForm(String fname,String lname,String email,String tel,String pwrd,String conpwrd) {
+
+	// method to fill registration form
+	public void fillRegistrationForm(String fname, String lname, String email, String tel, String pwrd,
+			String conpwrd) {
 		firstNameInput.sendKeys(fname);
 		lastNameInput.sendKeys(lname);
 		emailInput.sendKeys(email);
@@ -64,59 +62,64 @@ public class RegistrationPage {
 		passwordInput.sendKeys(pwrd);
 		confirmPasswordInput.sendKeys(conpwrd);
 	}
-	
-	//method to check the privacy policy
+
+	// method to check the privacy policy
 	public void checkPrivacyPolicy() {
 		privacyCheckbox.click();
 	}
-	
-	//method to click continue button
+
+	// method to click continue button
 	public void clickContinueBtn() {
 		continueBtn.click();
 	}
-	
-	//method to verify page title
+
+	// method to verify page title
 	public void verifyPageTitle(String title) {
 		try {
-			String registerPageTitle=driver.getTitle();
-			Assert.assertEquals(title,registerPageTitle);
-		}catch(Exception e) {
+			String registerPageTitle = driver.getTitle();
+			Assert.assertEquals(title, registerPageTitle);
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 	}
-	
-	//method to click login link
+
+	// method to click login link
 	public void clickLoginLink() {
 		try {
-			WebElement loginLink=driver.findElement(By.linkText("login page"));
-			loginLink.click();	
-		}
-		catch(Exception e) {
+			WebElement loginLink = driver.findElement(By.linkText("login page"));
+			loginLink.click();
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	//method verify warning visibility
+
+	// method verify warning visibility
 	public void verifyWarningVisibility() {
 		Assert.assertTrue(warningDiv.isDisplayed());
 	}
-	
-	//method to verify warning visibility
+
+	// method to verify warning visibility
 	public boolean verifyConfirmPasswordWarning() {
 		WebElement warningDiv = driver.findElement(By.xpath("//*[@id='content']/form/fieldset[2]/div[2]/div/div"));
 		return warningDiv.isDisplayed();
 	}
-	
-	//method to verify warning visibility 
+
+	// method to verify warning visibility
 	public boolean verifyFirstNameWarning() {
 		WebElement warningDiv = driver.findElement(By.xpath("//*[@id='account']/div[2]/div/div"));
 		return warningDiv.isDisplayed();
 	}
-	
-	//method to verify warning visibility
+
+	// method to verify warning visibility
 	public boolean verifyLastNameWarning() {
 		WebElement warningDiv = driver.findElement(By.xpath("//*[@id='account']/div[3]/div/div"));
+		return warningDiv.isDisplayed();
+	}
+
+	// method to verify warning visibility
+	public boolean verifyTelephoneWarning() {
+		WebElement warningDiv = driver.findElement(By.xpath("//*[@id='account']/div[5]/div/div"));
 		return warningDiv.isDisplayed();
 	}
 }
--- src/test/java/com/team3/miniproject/testcases/registration/RegistrationTestSetup.java
+++ src/test/java/com/team3/miniproject/testcases/registration/RegistrationTestSetup.java
@@ -1,4 +1,5 @@
 package com.team3.miniproject.testcases.registration;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -20,190 +21,200 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import screenshot.ScreenShotCapture;
 
 public class RegistrationTestSetup {
-	
+
 	RegistrationPage rg_object;
 	WebDriver driver;
-	String baseUrl="http://localhost/opencartsite/index.php?route=account/register";
-	RegistrationData rd=new RegistrationData();			
-	ScreenShotCapture s; 
-  
-	//Should Pass
-	@Test
-  public void testCase001() {
-	  try {
-		  ArrayList<ArrayList<String>> myData=rd.userData();
-		  rg_object=new RegistrationPage(driver);
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.fillRegistrationForm(myData.get(0).get(0), myData.get(0).get(1), myData.get(0).get(2), myData.get(0).get(3), myData.get(0).get(4), myData.get(0).get(5));
-		  rg_object.clickContinueBtn();
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.verifyWarningVisibility();
-	  }catch(Exception e) {
-		  System.out.println(e);
-	  }
-  }
-	
-	//should Pass
-  @Test
-  public void testCase002() {
-	  try {
-		  ArrayList<ArrayList<String>> myData=rd.userData();
-		  rg_object=new RegistrationPage(driver);
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.fillRegistrationForm(myData.get(1).get(0), myData.get(1).get(1), myData.get(1).get(2), myData.get(1).get(3), myData.get(1).get(4), myData.get(1).get(5));
-		  rg_object.clickContinueBtn();
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.verifyWarningVisibility();
-	  }catch(Exception e) {
-		  System.out.println(e);
-	  }
-  }
-  
-  //Should Pass
-  @Test
-  public void testCase003() {
-	  try {	
-		  ArrayList<ArrayList<String>> myData=rd.userData();
-		  rg_object=new RegistrationPage(driver);
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.fillRegistrationForm(myData.get(2).get(0), myData.get(2).get(1), myData.get(2).get(2), myData.get(2).get(3), myData.get(2).get(4), myData.get(2).get(5));
-		  rg_object.clickContinueBtn();
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.verifyWarningVisibility();
-	  }catch(Exception e) {
-		  System.out.println(e);
-	  }
-  }
-	
-  @Test(enabled=false)
-  public void testCase004() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Deeksha","123", "1.1@example.com", "123", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Your Account Has Been Created!");
-	  Assert.assertTrue(rg_object.verifyConfirmPasswordWarning());
-  }
- 
-  //Should Pass
-  @Test
-  public void testCase005() {
-	  try {
-		  ArrayList<ArrayList<String>> myData=rd.userData();
-		  rg_object=new RegistrationPage(driver);
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.fillRegistrationForm(myData.get(3).get(0), myData.get(3).get(1), myData.get(3).get(2), myData.get(3).get(3), myData.get(3).get(4), myData.get(3).get(5));
-		  rg_object.checkPrivacyPolicy();
-		  rg_object.clickContinueBtn();
-		  WebDriverWait w=new WebDriverWait(driver,3);
-		  w.until(ExpectedConditions.titleContains("Your Account Has Been Created!"));
-		  rg_object.verifyPageTitle("Your Account Has Been Created!");
-	  }catch(Exception e) {
-		  System.out.println(e);
-	  }
-  }
-  
-  //should Fail
-  @Test
-  public void testCase008() {
-	  try {
-		  ArrayList<ArrayList<String>> myData=rd.userData();
-		  rg_object=new RegistrationPage(driver);
-		  rg_object.verifyPageTitle("Register Account");
-		  rg_object.fillRegistrationForm(myData.get(4).get(0), myData.get(4).get(1), myData.get(4).get(2), myData.get(4).get(3), myData.get(4).get(4), myData.get(4).get(5));
-		  rg_object.checkPrivacyPolicy();
-		  rg_object.clickContinueBtn();
-		  WebDriverWait w=new WebDriverWait(driver,3);
-		  w.until(ExpectedConditions.titleContains("Your Account Has Been Created!"));
-		  rg_object.verifyPageTitle("Register Account");
-	  }catch(Exception e) {
-		  System.out.println(e);
-	  }
-  }
-  
-  
-  @Test(enabled=false)
-  public void testCase009() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.clickLoginLink();
-	  rg_object.verifyPageTitle("Account Login");
-  }
-  
-  @Test(enabled=false)
-  public void testCase006() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Raunak", "Naik","demo@example.com", "2123434565", "testing123", "test4567");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  Assert.assertTrue(rg_object.verifyConfirmPasswordWarning());
-  }
-  
-  @Test(enabled=false)
-  public void testCase007() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Raunak", "Naik","demo1@example.com", "2123434565", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.verifyWarningVisibility();
-  }
-  
-  @Test(enabled=false)
-  public void testCase010() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("", "Naik","demo2@example.com", "2123434565", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  Assert.assertTrue(rg_object.verifyFirstNameWarning());
-  }
-  
-  @Test(enabled=false)
-  public void testCase011() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Raunak", "","demo3@example.com", "2123434565", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  Assert.assertTrue(rg_object.verifyLastNameWarning());
-  }
-  
-  @Test(enabled=false)
-  public void testCase012() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Raunakabcdefghijklmnopqrstuvwxyza", "Naik","demo4@example.com", "2123434565", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  Assert.assertTrue(rg_object.verifyFirstNameWarning());
-  }
-  
-  @Test(enabled=false)
-  public void testCase013() {
-	  rg_object=new RegistrationPage(driver);
-	  rg_object.verifyPageTitle("Register Account");
-	  rg_object.fillRegistrationForm("Raunak", "Naikabcdefghijklmnopqrstuvwxyzabc","demo5@example.com", "2123434565", "testing123", "testing123");
-	  rg_object.clickContinueBtn();
-	  rg_object.verifyPageTitle("Register Account");
-	  Assert.assertTrue(rg_object.verifyLastNameWarning());
-  }
-  
-  @BeforeMethod
-  public void beforeMethod() {
-	  WebDriverManager.chromedriver().setup();
-	  driver=new ChromeDriver();
-	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	  driver.get(baseUrl);
-	  driver.manage().window().maximize();
-  }
+	String baseUrl = "http://localhost/opencartsite/index.php?route=account/register";
+	RegistrationData rd = new RegistrationData();
+	ScreenShotCapture s;
 
-  @AfterMethod
-  public void afterMethod(ITestResult testResult) throws IOException {
-	  if(!testResult.isSuccess()) {
-		  s=new ScreenShotCapture(driver);
-		  s.captureScreenshot(testResult.getName()+".png");
-	  }
-	  driver.quit();
-  }
+	// Should Pass
+	@Test
+	public void testCase001() {
+		try {
+			ArrayList<ArrayList<String>> myData = rd.userData();
+			rg_object = new RegistrationPage(driver);
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.fillRegistrationForm(myData.get(0).get(0), myData.get(0).get(1), myData.get(0).get(2),
+					myData.get(0).get(3), myData.get(0).get(4), myData.get(0).get(5));
+			rg_object.clickContinueBtn();
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.verifyWarningVisibility();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	// should Pass
+	@Test
+	public void testCase002() {
+		try {
+			ArrayList<ArrayList<String>> myData = rd.userData();
+			rg_object = new RegistrationPage(driver);
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.fillRegistrationForm(myData.get(1).get(0), myData.get(1).get(1), myData.get(1).get(2),
+					myData.get(1).get(3), myData.get(1).get(4), myData.get(1).get(5));
+			rg_object.clickContinueBtn();
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.verifyWarningVisibility();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	// Should Pass
+	@Test
+	public void testCase003() {
+		try {
+			ArrayList<ArrayList<String>> myData = rd.userData();
+			rg_object = new RegistrationPage(driver);
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.fillRegistrationForm(myData.get(2).get(0), myData.get(2).get(1), myData.get(2).get(2),
+					myData.get(2).get(3), myData.get(2).get(4), myData.get(2).get(5));
+			rg_object.clickContinueBtn();
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.verifyWarningVisibility();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test(enabled = false)
+	public void testCase004() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Deeksha", "123", "1.1@example.com", "123", "testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Your Account Has Been Created!");
+		Assert.assertTrue(rg_object.verifyConfirmPasswordWarning());
+	}
+
+	// Should Pass
+	@Test
+	public void testCase005() {
+		try {
+			ArrayList<ArrayList<String>> myData = rd.userData();
+			rg_object = new RegistrationPage(driver);
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.fillRegistrationForm(myData.get(3).get(0), myData.get(3).get(1), myData.get(3).get(2),
+					myData.get(3).get(3), myData.get(3).get(4), myData.get(3).get(5));
+			rg_object.checkPrivacyPolicy();
+			rg_object.clickContinueBtn();
+			WebDriverWait w = new WebDriverWait(driver, 3);
+			w.until(ExpectedConditions.titleContains("Your Account Has Been Created!"));
+			rg_object.verifyPageTitle("Your Account Has Been Created!");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	// should Fail
+	@Test
+	public void testCase008() {
+		try {
+			ArrayList<ArrayList<String>> myData = rd.userData();
+			rg_object = new RegistrationPage(driver);
+			rg_object.verifyPageTitle("Register Account");
+			rg_object.fillRegistrationForm(myData.get(4).get(0), myData.get(4).get(1), myData.get(4).get(2),
+					myData.get(4).get(3), myData.get(4).get(4), myData.get(4).get(5));
+			rg_object.checkPrivacyPolicy();
+			rg_object.clickContinueBtn();
+			WebDriverWait w = new WebDriverWait(driver, 3);
+			w.until(ExpectedConditions.titleContains("Your Account Has Been Created!"));
+			rg_object.verifyPageTitle("Register Account");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test(enabled = false)
+	public void testCase009() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.clickLoginLink();
+		rg_object.verifyPageTitle("Account Login");
+	}
+
+	// should pass
+	@Test(enabled = false)
+	public void testCase006() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Raunak", "Naik", "demo@example.com", "2123434565", "testing123", "test4567");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		Assert.assertTrue(rg_object.verifyConfirmPasswordWarning());
+	}
+
+	// should pass
+	@Test(enabled = false)
+	public void testCase007() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Raunak", "Naik", "demo1@example.com", "2123434565", "testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.verifyWarningVisibility();
+	}
+
+	// should pass
+	@Test(enabled = false)
+	public void testCase010() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("", "Naik", "demo2@example.com", "2123434565", "testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		Assert.assertTrue(rg_object.verifyFirstNameWarning());
+	}
+
+	// should pass
+	@Test(enabled = false)
+	public void testCase011() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Raunak", "", "demo3@example.com", "12", "testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		Assert.assertTrue(rg_object.verifyLastNameWarning());
+	}
+
+	@Test(enabled = false)
+	public void testCase012() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Raunakabcdefghijklmnopqrstuvwxyza", "Naik", "demo4@example.com", "2123434565",
+				"testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		Assert.assertTrue(rg_object.verifyFirstNameWarning());
+	}
+
+	@Test(enabled = false)
+	public void testCase013() {
+		rg_object = new RegistrationPage(driver);
+		rg_object.verifyPageTitle("Register Account");
+		rg_object.fillRegistrationForm("Raunak", "Naikabcdefghijklmnopqrstuvwxyzabc", "demo5@example.com", "2123434565",
+				"testing123", "testing123");
+		rg_object.clickContinueBtn();
+		rg_object.verifyPageTitle("Register Account");
+		Assert.assertTrue(rg_object.verifyLastNameWarning());
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult testResult) throws IOException {
+		if (!testResult.isSuccess()) {
+			s = new ScreenShotCapture(driver);
+			s.captureScreenshot(testResult.getName() + ".png");
+		}
+		driver.quit();
+	}
 }
