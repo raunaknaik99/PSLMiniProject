ddadb50e941398630b6206e80744993b5d783e31
errolcostabir <errolcostabir98@gmail.com>
2021-11-18 13:26:24 +0530

    cartTestSetup file complete with comments

--- src/test/java/com/team3/miniproject/testcases/cart/CartTestSetup.java
+++ src/test/java/com/team3/miniproject/testcases/cart/CartTestSetup.java
@@ -33,16 +33,22 @@ public class CartTestSetup {
 	ExtentReports report;
 	ExtentTest test;
 	
-	//TC_OC_CART_008
-  @Test(enabled=false)
+  //TC_OC_CART_008
+  @Test
   public void testCase008() {
 	  test.log(LogStatus.INFO, "TC_OC_CART_008-Add product with customizable features to cart but leave some of the required fields in the features form blank ");
-	  hpc_object=new AppleCinema(driver);
+	  hpc_object=new AppleCinema(driver); //create a new instance of AppleCinema Class
+	  //click on add to cart button on home page
 	  hpc_object.clickAppleCinemaCart("//button[@onclick=\"cart.add('42');\"]");
+	  //click on radio button
 	  hpc_object.clickRadioButton("//input[@name='option[218]' and @value='6']");
+	  //click on checkbox
 	  hpc_object.clickCheckbox("//input[@name='option[223][]' and @value='10']");
+	  //click on add to cart button
 	  hpc_object.clickAddToCart("button-cart");
+	  //Assert if warning is visible
 	  hpc_object.checkMandatoryFieldsWarning("//div[@class='text-danger']");
+	  //Check if test Passed or failed
 	  if(hpc_object.checkPageTitle().equals("Apple Cinema 30")) {
 		  test.log(LogStatus.PASS, "Test Passed- Title Matched");
 	  }else {
@@ -50,38 +56,36 @@ public class CartTestSetup {
 	  }
   }
   
-//TC_OC_CART_009
+  //TC_OC_CART_009
   @Test
   public void testCase009() throws InterruptedException, IOException {
 	  test.log(LogStatus.INFO, "TC_OC_CART_009-To verify that new window opens when user clicks the like button");
-	  hpc_object=new AppleCinema(driver);
+	  hpc_object=new AppleCinema(driver); //create an instance of AppleCart class
+	  //click on cinema cart
 	  hpc_object.clickAppleCinemaCart("//button[@onclick=\"cart.add('42');\"]");
-	 
+	  //wait till title loads
 	  WebDriverWait w =new WebDriverWait(driver, 5);
 	  w.until(ExpectedConditions.titleIs("Apple Cinema 30"));
-	  
+	  //get parent window handle
 	  String parentWindowHandle=driver.getWindowHandle();
-	  //Assert.assertEquals("Apple Cinema 30", driver.getTitle());
 	  if(driver.getTitle().equals("Apple Cinema 30")) {
 		  test.log(LogStatus.PASS, "Test Passed- Title Matched");
 	  }
 	  else {
 	      test.log(LogStatus.FAIL, "Test Failed- Title Mismatched");
 	  }
+	  //click on like btn
 	  hpc_object.clickFbLikeBtn();
-	  
+	  //get all the window handles
 	  Set<String> childWindowHandles=driver.getWindowHandles();
-	  //System.out.println(childWindowHandles);
 	  Iterator<String> itr=childWindowHandles.iterator();  
 	  while(itr.hasNext()) {
 		  String childWindow=itr.next();
+		  //switch over to child handle
 		  if(!parentWindowHandle.equals(childWindow)) {
 			  driver.switchTo().window(childWindow);
-			  //String childPageTitle=driver.getTitle();
 			  w.until(ExpectedConditions.titleIs("Facebook"));
 			  test.log(LogStatus.INFO, "Switched to child window handle");
-			  //System.out.println("child window handle");
-			  //Assert.assertEquals("Facebook", childPageTitle);
 			  if(driver.getTitle().equals("Facebook")) {
 				  test.log(LogStatus.PASS, "Test Passed- Child window Title matched");
 			  }else {
@@ -94,9 +98,10 @@ public class CartTestSetup {
 		  }
 	  }
 	  Thread.sleep(3000);//only to visualize the going back to parent window
+	  //switch back to parent window handle
 	  driver.switchTo().window(parentWindowHandle);
-	 // Assert.assertEquals("Apple Cinema 30", driver.getTitle());
 	  test.log(LogStatus.INFO, "Switched to parent window handle");
+	  //verify is test passed or failed
 	  if(driver.getTitle().equals("Apple Cinema 30")) {
 		  test.log(LogStatus.PASS, "Test Passed- Parent Window Title Matched");
 	  }
@@ -105,41 +110,37 @@ public class CartTestSetup {
 	  }
   }
   
-//TC_OC_CART_010
+  //TC_OC_CART_010
   @Test(enabled=false)
   public void testCase010() throws InterruptedException, IOException {
 	  test.log(LogStatus.INFO, "TC_OC_CART_010-To verify that new window opens when user clicks the Tweet button");
-	  hpc_object=new AppleCinema(driver);
-	  hpc_object.clickAppleCinemaCart("//button[@onclick=\"cart.add('42');\"]");
 	  
+	  hpc_object=new AppleCinema(driver); //create new instance of AppleCinema class
+	  //click on cinema cart
+	  hpc_object.clickAppleCinemaCart("//button[@onclick=\"cart.add('42');\"]");
+	  //wait till title loads
 	  WebDriverWait w =new WebDriverWait(driver, 3);
 	  w.until(ExpectedConditions.titleIs("Apple Cinema 30"));
-	  
+	  //get parent window handle
 	  String parentWindowHandle=driver.getWindowHandle();
-	  //System.out.println(parentWindowHandle);
-	  //Assert.assertEquals("Apple Cinema 30", driver.getTitle());
 	  if(driver.getTitle().equals("Apple Cinema 30")) {
 		  test.log(LogStatus.PASS, "Test Passed- Title Matched");
 	  }
 	  else {
 	      test.log(LogStatus.FAIL, "Test Failed- Title Mismatched");
-	  }
-	  
+	  }	  
+	  //click on tweet btn
 	  hpc_object.clickTweetBtn();
-	  
+	  //get child window handles
 	  Set<String> childWindowHandles=driver.getWindowHandles();
-	 // System.out.println(childWindowHandles);
 	  Iterator<String> itr=childWindowHandles.iterator();  
 	  while(itr.hasNext()) {
 		  String childWindow=itr.next();
+		  //switch to child window
 		  if(!parentWindowHandle.equals(childWindow)) {
 			  driver.switchTo().window(childWindow);
-			  //String childPageTitle=driver.getTitle();
 			  w.until(ExpectedConditions.titleIs("Twitter"));
-			  //Assert.assertEquals("Twitter", childPageTitle);
 			  test.log(LogStatus.INFO, "Switched to child window handle");
-			  //System.out.println("child window handle");
-			  //Assert.assertEquals("Facebook", childPageTitle);
 			  if(driver.getTitle().equals("Twitter")) {
 				  test.log(LogStatus.PASS, "Test Passed- Child window Title matched");
 			  }else {
@@ -152,8 +153,8 @@ public class CartTestSetup {
 		  }
 	  }
 	  Thread.sleep(3000); //only to visualize the going back to parent window
+	  //switch back to parent window
 	  driver.switchTo().window(parentWindowHandle);
-	  //Assert.assertEquals("Apple Cinema 30", driver.getTitle());
 	  test.log(LogStatus.INFO, "Switched to parent window handle");
 	  if(driver.getTitle().equals("Apple Cinema 30")) {
 		  test.log(LogStatus.PASS, "Test Passed- Parent Window Title Matched");
