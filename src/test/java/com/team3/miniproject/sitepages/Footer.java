d8302d3203aa190f95e2e44a89966b3287afe1be
errolcostabir <errolcostabir98@gmail.com>
2021-11-19 11:09:12 +0530

    modified footer POM and test case file

--- src/test/java/com/team3/miniproject/sitepages/Footer.java
+++ src/test/java/com/team3/miniproject/sitepages/Footer.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 //POM for footer links. All the methods to click the various links are coded here.
@@ -11,192 +13,247 @@ import org.testng.Assert;
 public class Footer {
 	WebDriver driver;
 	JavascriptExecutor js;
+	
+	//contact us link element 
+	@FindBy(linkText = "Contact Us")
+	WebElement contactUsLink;
+	
+	//site map link element
+	@FindBy(linkText = "Site Map")
+	WebElement siteMapLink;
+	
+	//about us link element
+	@FindBy(linkText = "About Us")
+	WebElement aboutUsLink;
+	
+	//delivery information link element
+	@FindBy(linkText = "Delivery Information")
+	WebElement deliveryInformation;
+	
+	//privacy policy link element
+	@FindBy(linkText = "Privacy Policy")
+	WebElement privacyPolicy;
 
+	//returns link element
+	@FindBy(linkText = "Returns")
+	WebElement returns;
+	
+	//brands link element
+	@FindBy(linkText ="Brands")
+	WebElement brands;
+	
+	//affiliate link element
+	@FindBy(linkText = "Affiliate")
+	WebElement afl;
+	
+	//terms and conditions link element
+	@FindBy(linkText = "Terms & Conditions")
+	WebElement tnc;
+	
+	//newsletter link element
+	@FindBy(linkText = "Newsletter")
+	WebElement newsl;
+	
+	//my account link element
+	@FindBy(linkText = "My Account")
+	WebElement myAcc;
+	
 	public Footer(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 		js = (JavascriptExecutor) driver;
 	}
 
 	// Click the contact us link
 	public void getContactUs() {
-		try {
-			WebElement contactUsLink = driver.findElement(By.linkText("Contact Us"));
-			js.executeScript("arguments[0].scrollIntoView();", contactUsLink);
-			if (contactUsLink.isDisplayed()) {
-				Assert.assertEquals(contactUsLink.getText(), "Contact Us");
-				contactUsLink.click();
-				String contactUsPageTitle = driver.getTitle();
-				Assert.assertEquals("Contact Us", contactUsPageTitle);
-			} else {
-				System.out.println("Link Not Present");
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		js.executeScript("arguments[0].scrollIntoView();", contactUsLink);
+		contactUsLink.click();
+			//WebElement contactUsLink = driver.findElement(By.linkText("Contact Us"));
+			
+//			if (contactUsLink.isDisplayed()) {
+//				Assert.assertEquals(contactUsLink.getText(), "Contact Us");
+//				contactUsLink.click();
+//				String contactUsPageTitle = driver.getTitle();
+//				Assert.assertEquals("Contact Us", contactUsPageTitle);
+//			} else {
+//				System.out.println("Link Not Present");
+//			}
+		
 	}
 
 	// click the site map link
 	public void getSiteMap() {
-		try {
-			WebElement siteMapLink = driver.findElement(By.linkText("Site Map"));
+		//try {
+			//WebElement siteMapLink = driver.findElement(By.linkText("Site Map"));
 			js.executeScript("arguments[0].scrollIntoView();", siteMapLink);
-			if (siteMapLink.isDisplayed()) {
-				Assert.assertEquals(siteMapLink.getText(), "Site Map");
-				siteMapLink.click();
-				String siteMapPageTitle = driver.getTitle();
-				Assert.assertEquals("Site Map", siteMapPageTitle);
-			} else {
-				System.out.println("Link Not Present");
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+			siteMapLink.click();
+//			if (siteMapLink.isDisplayed()) {
+//				Assert.assertEquals(siteMapLink.getText(), "Site Map");
+//				siteMapLink.click();
+//				String siteMapPageTitle = driver.getTitle();
+//				Assert.assertEquals("Site Map", siteMapPageTitle);
+//			} else {
+//				System.out.println("Link Not Present");
+//			}
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
 	}
 
 	// click the about us link
 	public void getAboutUs() {
-		try {
-			WebElement aboutUsLink = driver.findElement(By.linkText("About Us"));
+		//try {
+			//WebElement aboutUsLink = driver.findElement(By.linkText("About Us"));
+		
 			js.executeScript("arguments[0].scrollIntoView();", aboutUsLink);
-			if (aboutUsLink.isDisplayed()) {
-				Assert.assertEquals(aboutUsLink.getText(), "About Us");
-				aboutUsLink.click();
-				String aboutUsPageTitle = driver.getTitle();
-				Assert.assertEquals("About Us", aboutUsPageTitle);
-			} else {
-				System.out.println("Link Not Present");
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+			aboutUsLink.click();
+//			if (aboutUsLink.isDisplayed()) {
+//				Assert.assertEquals(aboutUsLink.getText(), "About Us");
+//				aboutUsLink.click();
+//				String aboutUsPageTitle = driver.getTitle();
+//				Assert.assertEquals("About Us", aboutUsPageTitle);
+//			} else {
+//				System.out.println("Link Not Present");
+//			}
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
 	}
 
 	// click the delivery information link
 	public void getDeliveryInformation() {
-		try {
-			WebElement deliveryInformation = driver.findElement(By.linkText("Delivery Information"));
+		//try {
+		//	WebElement deliveryInformation = driver.findElement(By.linkText("Delivery Information"));
 			js.executeScript("arguments[0].scrollIntoView();", deliveryInformation);
-			if (deliveryInformation.isDisplayed()) {
-				Assert.assertEquals(deliveryInformation.getText(), "Delivery Information");
-				deliveryInformation.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Delivery Information", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			deliveryInformation.click();
+//			if (deliveryInformation.isDisplayed()) {
+//				Assert.assertEquals(deliveryInformation.getText(), "Delivery Information");
+//				deliveryInformation.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Delivery Information", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the privacy policy link
 	public void getPrivacyPolicy() {
-		try {
-			WebElement privacyPolicy = driver.findElement(By.linkText("Privacy Policy"));
+		//try {
+			//WebElement privacyPolicy = driver.findElement(By.linkText("Privacy Policy"));
 			js.executeScript("arguments[0].scrollIntoView();", privacyPolicy);
-			if (privacyPolicy.isDisplayed()) {
-				Assert.assertEquals(privacyPolicy.getText(), "Privacy Policy");
-				privacyPolicy.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Privacy Policy", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			privacyPolicy.click();
+//			if (privacyPolicy.isDisplayed()) {
+//				Assert.assertEquals(privacyPolicy.getText(), "Privacy Policy");
+//				privacyPolicy.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Privacy Policy", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the returns link
 	public void getReturns() {
-		try {
-			WebElement returns = driver.findElement(By.linkText("Returns"));
+		//try {
+			//WebElement returns = driver.findElement(By.linkText("Returns"));
 			js.executeScript("arguments[0].scrollIntoView();", returns);
-			if (returns.isDisplayed()) {
-				Assert.assertEquals(returns.getText(), "Returns");
-				returns.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Product Returns", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			returns.click();
+//			if (returns.isDisplayed()) {
+//				Assert.assertEquals(returns.getText(), "Returns");
+//				returns.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Product Returns", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the brandspage link
 	public void getBrandsPage() {
-		try {
-			WebElement brands = driver.findElement(By.linkText("Brands"));
+		//try {
+		//	WebElement brands = driver.findElement(By.linkText("Brands"));
 			js.executeScript("arguments[0].scrollIntoView();", brands);
-			if (brands.isDisplayed()) {
-				Assert.assertEquals(brands.getText(), "Brands");
-				brands.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Find Your Favorite Brand", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			brands.click();
+//			if (brands.isDisplayed()) {
+//				Assert.assertEquals(brands.getText(), "Brands");
+//				brands.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Find Your Favorite Brand", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the affiliate page link
 	public void getAffiliatePage() {
-		try {
-			WebElement afl = driver.findElement(By.linkText("Affiliate"));
+		//try {
+		//	WebElement afl = driver.findElement(By.linkText("Affiliate"));
 			js.executeScript("arguments[0].scrollIntoView();", afl);
-			if (afl.isDisplayed()) {
-				Assert.assertEquals(afl.getText(), "Affiliate");
-				afl.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Affiliate Program", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			afl.click();
+//			if (afl.isDisplayed()) {
+//				Assert.assertEquals(afl.getText(), "Affiliate");
+//				afl.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Affiliate Program", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the terms and conditions link
 	public void getTermsAndConditions() {
-		try {
-			WebElement tnc = driver.findElement(By.linkText("Terms & Conditions"));
+		//try {
+			//WebElement tnc = driver.findElement(By.linkText("Terms & Conditions"));
 			js.executeScript("arguments[0].scrollIntoView();", tnc);
-			if (tnc.isDisplayed()) {
-				Assert.assertEquals(tnc.getText(), "Terms & Conditions");
-				tnc.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Terms & Conditions", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			tnc.click();
+//			if (tnc.isDisplayed()) {
+//				Assert.assertEquals(tnc.getText(), "Terms & Conditions");
+//				tnc.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Terms & Conditions", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the news letter link
 	public void getNewsletter() {
-		try {
-			WebElement newsl = driver.findElement(By.linkText("Newsletter"));
+		//try {
+			//WebElement newsl = driver.findElement(By.linkText("Newsletter"));
 			js.executeScript("arguments[0].scrollIntoView();", newsl);
-			if (newsl.isDisplayed()) {
-				Assert.assertEquals(newsl.getText(), "Newsletter");
-				newsl.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Account Login", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			newsl.click();
+//			if (newsl.isDisplayed()) {
+//				Assert.assertEquals(newsl.getText(), "Newsletter");
+//				newsl.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Account Login", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	// click the My Account link
 	public void getMyAccount() {
-		try {
-			WebElement myAcc = driver.findElement(By.linkText("My Account"));
+		//try {
+		//	WebElement myAcc = driver.findElement(By.linkText("My Account"));
 			js.executeScript("arguments[0].scrollIntoView();", myAcc);
-			if (myAcc.isDisplayed()) {
-				Assert.assertEquals(myAcc.getText(), "My Account");
-				myAcc.click();
-				String actualTitle = driver.getTitle();
-				Assert.assertEquals("Account Login", actualTitle);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			myAcc.click();
+//			if (myAcc.isDisplayed()) {
+//				Assert.assertEquals(myAcc.getText(), "My Account");
+//				myAcc.click();
+//				String actualTitle = driver.getTitle();
+//				Assert.assertEquals("Account Login", actualTitle);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
