c72c53d7d89cc123df2a256113b98a64724e57b1
deeksha0512 <92747132+deeksha0512@users.noreply.github.com>
2021-11-21 19:02:21 +0530

    Update LoginTestCases.java

--- src/test/java/com/team3/miniproject/testcases/login/LoginTestCases.java
+++ src/test/java/com/team3/miniproject/testcases/login/LoginTestCases.java
@@ -15,39 +15,76 @@ public class LoginTestCases {
 
 	@Test
 	public void verifyTestCase001() throws IOException {
-		login1.forgotPassword();
-
-		reader.readExcel("src\\sitepages", "loginDDT.xlsx", "Login");
-		reader.i = 10;
-		String emailId = reader.getEmailId();
-		login1.enterEmail(emailId);
-
-		String actualTitle = driver.getTitle();
-		System.out.println(actualTitle);
+		test.log(LogStatus.INFO,"TC_OC_FP_001 - using forgotten password with invalid email id");
+		s = new ScreenShotCapture(driver);
+		  
+		  login1.forgotPassword();
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+		  reader.readExcel("src\\test\\resources", "loginDDT.xlsx", "Login");
+		  reader.i = 10;
+		  String emailId = reader.getEmailId();
+		  login1.enterEmail(emailId);
+		 
+		  actualTitle = login1.driver.getTitle();
+		  expectedTitle = "My Account";
+			if (actualTitle.equals(expectedTitle))
+				test.log(LogStatus.PASS,
+						"Test Passed - User entered correct email Id and an email with confirmation link has been sent.");
+			else
+				test.log(LogStatus.FAIL,
+						test.addScreenCapture(s.captureScreenshot("\\Login\\testCase009_" + timeStamp + ".PNG"))
+								+ "Test Failed - The E-Mail Address was not found in our records, please try again!.");
+		  	Assert.assertEquals(actualTitle, expectedTitle);
+		
 	}
 
 	@Test
 	public void verifyTestCase002() throws InterruptedException, IOException {
-		login1.forgotPassword();
-
-		reader.readExcel("src\\sitepages", "loginDDT.xlsx", "Login");
-		reader.i = 11;
-		String emailId = reader.getEmailId();
-		login1.enterEmail(emailId);
-
-		String actualTitle = driver.getTitle();
-		System.out.println(actualTitle);
+		 test.log(LogStatus.INFO,"TC_OC_FP_002 - using forgotten password with valid email id");
+		 s = new ScreenShotCapture(driver);
+			 
+	  	 login1.forgotPassword();
+		  
+	  	 reader.readExcel("src\\test\\resources", "loginDDT.xlsx", "Login");
+		  reader.i = 11;
+		  String emailId = reader.getEmailId();
+		  login1.enterEmail(emailId);
+		 
+		  actualTitle = login1.driver.getTitle();
+		  expectedTitle = "My Account";
+			if (actualTitle.equals(expectedTitle))
+				test.log(LogStatus.PASS,
+						"Test Passed - User entered correct email Id and an email with confirmation link has been sent.");
+			else
+				test.log(LogStatus.FAIL,
+						test.addScreenCapture(s.captureScreenshot("\\Login\\testCase010_" + timeStamp + ".PNG"))
+								+ "Test Failed - The E-Mail Address was not found in our records, please try again!.");
+		  	Assert.assertEquals(actualTitle, expectedTitle);
 	}
 
 	@Test
 	public void logoutTestCase001() throws InterruptedException, IOException {
-		login1.navigateToLogin();
-		reader.readExcel("src\\sitepages", "loginDDT.xlsx", "Login");
-		reader.i = 11;
-		String emailId = reader.getEmailId();
-		String password = reader.getPassword();
-		login1.login(emailId, password);
-
-		login1.logout();
+		test.log(LogStatus.INFO,"TC_OC_LO_001 - To Verify that user does not get logged back in automatically on clicking continue after logging out.");
+		 s = new ScreenShotCapture(driver);
+			 
+		 login1.navigateToLogin();
+		  reader.readExcel("src\\sitepages", "loginDDT.xlsx", "Login");
+		  reader.i = 11;
+		  String emailId = reader.getEmailId();
+		  String password = reader.getPassword();
+		  login1.login(emailId, password);
+		  
+		  login1.logout();
+		  actualTitle = login1.driver.getTitle();
+		  expectedTitle = "Account Logout";
+			if (actualTitle.equals(expectedTitle))
+				test.log(LogStatus.PASS,
+						"Test Passed - Account is logged out successfully.");
+			else
+				test.log(LogStatus.FAIL,
+						test.addScreenCapture(s.captureScreenshot("\\Login\\testCase011_" + timeStamp + ".PNG"))
+								+ "Test Failed - Account is not logged out.");
+		  	Assert.assertEquals(actualTitle, expectedTitle);
 	}
 }
